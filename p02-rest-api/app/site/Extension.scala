11637582abd3555784345ffa34058b644e315dea
Jose Ibanez <jose.ibanez@vodafone.com>
2021-02-14 22:34:54 +0100

    save

--- p02-rest-api/app/site/Extension.scala
+++ p02-rest-api/app/site/Extension.scala
@@ -1,17 +1,40 @@
 package site
 
+import models.{NewTodoListItem, TodoListItem}
+import play.api.libs.json.Json
+import play.api.libs.json._
+
 import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
 
 case class Extension(ext: String, display:String, model:String, template:String)
+case class ExportSite(id:String, name: String, city:String, extensionList: ListBuffer[Extension])
 
-class Site(name:String, city:String) {
+class Site( id:String, name:String, city:String) {
 
   private val extensionList = new mutable.ListBuffer[Extension]()
 
-  def addExtension(display: String, template: String)
+  implicit val extensionJson = Json.format[Extension]
+  implicit val exportSiteJson = Json.format[ExportSite]
+  //implicit val todoListJson = Json.format[TodoListItem]
+
+
+  def addExtension(extension: Extension) = {
+    extensionList += extension
+  }
+
+  def getExtensionList(): ListBuffer[Extension] = {
+    extensionList
+  }
 
   def generateExtList(range: Int, size: Int): List[Extension] = {
     List.range(range, range + size).map( x => Extension( s"$x", s"Extension $x", "Basic", "Basic" ))
+  }
+
+  def save(): Unit = {
+    val export1 = ExportSite(id, name, city, extensionList)
+    val exportStr = Json.toJson(export1)
+    println(s"exportStr: $exportStr")
   }
 
 }
