9634ef9875a1f4a791c7bac873f357c39bf0e826
Jose Ibanez <jose.ibanez@vodafone.com>
2021-02-14 22:34:28 +0100

    save

--- p02-rest-api/test/site/SiteSpec.scala
+++ p02-rest-api/test/site/SiteSpec.scala
@@ -9,11 +9,17 @@ import org.scalatest.FunSuite
 class SiteSpec extends FunSuite {
   test("site.SiteSpec.extension") {
     val ext200 = Extension("200", "Extension 200", "Basic", "Basic")
+    val ext201 = Extension("201", "Extension 201", "Basic", "Basic")
     println(ext200)
 
-    val site1 = new Site();
-    val list1 = site1.generateExtList(200,20)
-    println(list1)
-    assert(27 === 27)
+    val site1 = new Site( "001", "Office 1", "Madrid")
+    site1.addExtension(ext200)
+    site1.addExtension(ext201)
+    val list1 = site1.getExtensionList()
+    println(s"list1: $list1")
+    site1.save()
+
+
+    assert(27 === 27 )
   }
 }
